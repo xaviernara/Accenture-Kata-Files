82dc7a6106815f0ddbb80748fa2f899c116573cc
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2019-08-11 21:49:28 -0700

    rewrote/completed 1st test
    The test does a  check to see if  family  A wants the babysitter to babysit for 1 hour and returns the hourly rate for the babysitter's work

---
+++ src/BabySitter.java
@@ -0,0 +1,14 @@
+public class BabySitter {
+
+    public static int calcHourlyRate(String familyName){
+
+        int hourlyRateToBabysitForFamily = 0;
+
+        if (familyName == "A"){
+            return 30;
+        }
+        return hourlyRateToBabysitForFamily;
+    }
+
+
+}
--- src/BabysitterKataTests.java
+++ src/BabysitterKataTests.java
@@ -1,16 +1,26 @@
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-import static org.junit.jupiter.api.AssertEquals.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BabysitterKataTests {
 
-    @Test
-    public void babySitterWorksOneHourForFamilyAItReturnsHourlyRate{
-        BabySitter bs = new BabySitter();
-        assertEquals(30,bs.calcHourlyRate());
+    BabySitter bs;
+
+    @BeforeEach
+    public void setUp(){
+        bs = new BabySitter();
     }
+
+    @Test
+    public void babySitterWorksOneHourForAFamilyItReturnsHourlyRate(){
+        assertEquals(30,bs.calcHourlyRate("A"));
+    }
+
+
 
 
 }
