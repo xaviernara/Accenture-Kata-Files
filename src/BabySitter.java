a6ca491193e85320460f3e097a5f24cd57acb02f
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2019-08-13 21:55:20 -0700

    completed test to determine if babysitter is  avaliable based on a time range
    This test uses a random time frame to test the babysitter's availablity based on the allocated time frame for babysitting

--- src/BabySitter.java
+++ src/BabySitter.java
@@ -1,3 +1,8 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Random;
+
 public class BabySitter {
 
     public static int calcHourlyRate(String familyName){
@@ -10,5 +15,42 @@ public class BabySitter {
         return hourlyRateToBabysitForFamily;
     }
 
+    public static String determineWhichFamilyWantsBabysitter(String familyName){
 
+        return familyName;
+    }
+
+    public static void familyAMiscDetails(){
+
+        int numOfPets = 2;
+        int numOfKids = 3;
+
+        Random random = new Random();
+
+        LocalTime bedTime = LocalTime.of(random.nextInt(24), random.nextInt(60));
+
+    }
+
+
+
+
+    public boolean determineIfAvailableToBabysitForTimeRange(LocalTime babysitTimeStart, LocalTime babysitTimeStop) {
+
+        LocalTime startBabysittingTime = LocalTime.parse( "17:00" );
+        LocalTime stopBabysittingTime = LocalTime.parse( "04:00" );
+
+        if (babysitTimeStart.isAfter(startBabysittingTime) && babysitTimeStop.isAfter(stopBabysittingTime)){
+            return true;
+        }
+        else if (babysitTimeStart.isBefore(stopBabysittingTime) && babysitTimeStop.isAfter(startBabysittingTime)){
+            return true;
+        }
+
+        else if (startBabysittingTime.equals(babysitTimeStart) && stopBabysittingTime.equals(babysitTimeStop) ){
+            return true;
+        }
+
+        return false;
+
+    }
 }
--- src/BabysitterKataTests.java
+++ src/BabysitterKataTests.java
@@ -1,23 +1,60 @@
 
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BabysitterKataTests {
 
     BabySitter bs;
+    LocalTime time, time2;
+    Random random;
 
-    @BeforeEach
+    @Before
     public void setUp(){
         bs = new BabySitter();
     }
 
+    @Before
+    public void randomTime(){
+
+
+        time = LocalTime.of(random.nextInt(24), random.nextInt(60));
+        time2 = LocalTime.of(random.nextInt(24), random.nextInt(60));
+
+
+        //DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern(" hh:mm ");
+
+        //String formattedDate = time.format(myFormatObj);
+        //System.out.println("After formatting: " + formattedDate);
+    }
+
+
+
     @Test
     public void babySitterWorksOneHourForAFamilyItReturnsHourlyRate(){
         assertEquals(30,bs.calcHourlyRate("A"));
+    }
+
+    @Test
+    public void determineWhichHoursBabysitterCanWorkReturnBooleanIfAvailable(){
+        assertEquals(true, bs.determineIfAvailableToBabysitForTimeRange(time, time2) );
+        assertEquals(false, bs.determineIfAvailableToBabysitForTimeRange(time, time2) );
+
+        //assertEquals(false, bs.determineIfAvailableToBabysitForTimeRange(time, time2) );
+        //assertEquals(false, bs.determineIfAvailableToBabysitForTimeRange(time, time2) );
+    }
+
+    @Test
+    public void FamilyIsRequestingBabysitterReturnsHourlyRate(){
+
     }
 
 
